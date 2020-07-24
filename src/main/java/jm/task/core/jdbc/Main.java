7c9633b12827434a1bfc4a8f168ff4225199bd23
Konstantin Ovsyannikov <ovsyannikov-kv@yandex.ru>
2020-07-24 18:02:30 +0300

    final version for check

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,31 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        List<User> users = new ArrayList<>();
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("ivan", "ivanov", (byte) 17);
+        userService.saveUser("petr", "petrov", (byte) 18);
+        userService.saveUser("sidorov", "sidorov", (byte) 19);
+        userService.saveUser("pavel", "pavlov", (byte) 20);
+
+        users = userService.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+//        userService.removeUserById(1);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,60 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    protected static final String URL = "jdbc:mysql://localhost:3306/task113?useSSL=false";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "Kingobiloba@61bb";
+
+    public static String getUrl() {
+        return URL;
+    }
+
+    public static String getLogin() {
+        return LOGIN;
+    }
+
+    public static String getPassword() {
+        return PASSWORD;
+    }
+
+    public static void registerDriver() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection startConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(Util.getUrl(), Util.getLogin(), Util.getPassword());
+        } catch (SQLException throwables) {
+            System.out.println("Ошибка установления соединения с базой данных!");
+            throwables.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static Statement getStatement(Connection connection) {
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException throwables) {
+            System.out.println("Ошибка создания statement!");
+            throwables.printStackTrace();
+        }
+        return statement;
+    }
+
+    public static void closeConnections(Statement statement, Connection connection) {
+        try {
+            statement.close();
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
